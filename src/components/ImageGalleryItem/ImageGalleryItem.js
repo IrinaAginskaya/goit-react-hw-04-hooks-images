6b9ce08878c98f80b77a7ebb747f61932653d4c5
IrinaAginskaya <aginska@gmail.com>
2022-01-21 18:23:59 +0200

    delete item, add seachbar

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++
@@ -1,7 +0,0 @@
-// export default function ImageGalleryItem({ image: { id, webformatURL, largeImageURL } }) {
-//   return (
-//     <li class="gallery-item">
-//       <img src="" alt="" width="240" height="200" />
-//     </li>
-//   );
-// }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,50 +1,94 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { SearchBar } from './Searchbar.styled';
 import PropTypes from 'prop-types';
 
-export default class Searchbar extends Component {
-  state = {
-    image: '',
+export default function Searchbar({ onSubmit }) {
+  const [image, setImage] = useState('');
+
+  const handleNameChange = event => {
+    setImage(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = event => {
-    this.setState({ image: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.image.trim() === '') {
+    if (image.trim() === '') {
       alert('Введите название картинки');
       return;
     }
-    this.props.onSubmit(this.state.image);
-    this.setState({ image: '' });
+    onSubmit(image);
+    setImage('');
   };
 
-  render() {
-    return (
-      <SearchBar>
-        <form onSubmit={this.handleSubmit}>
-          <button type="submit" class="button">
-            <span>Search</span>
-          </button>
+  return (
+    <SearchBar>
+      <form onSubmit={handleSubmit}>
+        <button type="submit" class="button">
+          <span>Search</span>
+        </button>
 
-          <input
-            class="input"
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-            value={this.state.image}
-            onChange={this.handleNameChange}
-          />
-        </form>
-      </SearchBar>
-    );
-  }
+        <input
+          class="input"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+          value={image}
+          onChange={handleNameChange}
+        />
+      </form>
+    </SearchBar>
+  );
 }
+
 Searchbar.propTypes = {
   image: PropTypes.string,
   handleNameChange: PropTypes.func,
   handleSubmit: PropTypes.func,
 };
+
+// export default class Searchbar extends Component {
+//   state = {
+//     image: '',
+//   };
+
+//   handleNameChange = event => {
+//     this.setState({ image: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.image.trim() === '') {
+//       alert('Введите название картинки');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.image);
+//     this.setState({ image: '' });
+//   };
+
+//   render() {
+//     return (
+//       <SearchBar>
+//         <form onSubmit={this.handleSubmit}>
+//           <button type="submit" class="button">
+//             <span>Search</span>
+//           </button>
+
+//           <input
+//             class="input"
+//             type="text"
+//             autocomplete="off"
+//             autofocus
+//             placeholder="Search images and photos"
+//             value={this.state.image}
+//             onChange={this.handleNameChange}
+//           />
+//         </form>
+//       </SearchBar>
+//     );
+//   }
+// }
+// Searchbar.propTypes = {
+//   image: PropTypes.string,
+//   handleNameChange: PropTypes.func,
+//   handleSubmit: PropTypes.func,
+// };
