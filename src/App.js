7827d1a6c7bc1404641768270bb2ef8ee150f1cd
IrinaAginskaya <aginska@gmail.com>
2022-01-22 14:00:47 +0200

    add app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Button from './components/Button/Button';
 import Searchbar from './components/Searchbar/Searchbar';
 import Modal from './components/Modal/Modal';
@@ -8,90 +8,83 @@ import Loader from './components/Loader/Loader';
 import api from './Api/Api';
 import PropTypes from 'prop-types';
 
-export default class App extends Component {
-  state = {
-    image: '',
-    data: [],
-    status: 'idle',
-    page: 1,
-    error: null,
-    showModal: false,
-    currImg: {},
-  };
-  componentDidUpdate(prevProps, prevState) {
-    const { page, image } = this.state;
+export default function App() {
+  const [image, setImage] = useState('');
+  const [data, setData] = useState([]);
+  const [status, setStatus] = useState(false);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [currImg, setCurrImg] = useState({});
 
-    if (prevState.image !== image) {
-      this.setState({ status: 'pending', page: 1 });
-      api
-        .fetchImage(image, page)
-        .then(data => data.hits)
-        .then(images => {
-          this.setState({ data: images, status: 'resolved' });
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
+  useEffect(() => {
+    if (image !== '') {
+      setStatus(true);
     }
+  }, [image]);
 
-    if (prevState.page !== page) {
-      this.setState({ status: 'pending' });
-
-      api
-        .fetchImage(image, page)
-        .then(data => data.hits)
-        .then(images =>
-          this.setState(prevState => ({
-            data: [...prevState.data, ...images],
-            status: 'resolved',
-          })),
-        )
-        .catch(error => this.setState({ error, status: 'rejected' }));
+  useEffect(() => {
+    if (!image) {
+      return;
     }
-  }
-  handleFormSubmit = image => {
-    this.setState({ image });
+    fetchImage();
+  }, [image]);
+
+  const fetchImage = () => {
+    setStatus(true);
+
+    api
+      .fetchImage(image, page)
+      .then(
+        ({ hits }) => setData(prevImage => [...prevImage, ...hits]),
+        setPage(prevPage => prevPage + 1),
+      )
+      .catch(error => setError(error))
+      .finally(() => setStatus(false));
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const handleFormSubmit = image => {
+    setImage(image);
+    setData([]);
+    setPage(1);
   };
 
-  toggleModal = image => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-      currImg: image,
-    }));
+  const onLoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { status, data, currImg } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {status === 'idle' && <div>Waiting for your request</div>}
+  const toggleModal = currImg => {
+    setShowModal(showModal => !showModal);
+    setCurrImg(currImg);
+  };
 
-        {status === 'pending' && (
-          <div>
-            <Loader />
-          </div>
-        )}
+  return (
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
 
-        {status === 'resolved' && (
-          <div>
-            <ImageGallery data={data} onOpenModal={this.toggleModal} />
-            {data.length > 0 && <Button onLoadMore={this.onLoadMore} />}
-          </div>
-        )}
+      {status && (
+        <div>
+          <Loader />
+        </div>
+      )}
 
-        {status === 'rejected' && <div> Something went wrong</div>}
-        {this.state.showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={currImg.largeImageURL} alt={currImg.tags} />
-          </Modal>
-        )}
-      </div>
-    );
-  }
+      {
+        <div>
+          <ImageGallery data={data} onOpenModal={toggleModal} />
+          {data.length > 0 && <Button onLoadMore={onLoadMore} />}
+        </div>
+      }
+
+      {error && <div> Something went wrong</div>}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={currImg.largeImageURL} alt={currImg.tags} />
+        </Modal>
+      )}
+    </div>
+  );
 }
+
 App.propTypes = {
   image: PropTypes.string,
   data: PropTypes.array,
@@ -104,3 +97,129 @@ App.propTypes = {
   onLoadMore: PropTypes.func,
   toggleModal: PropTypes.func,
 };
+
+// export default class App extends Component {
+//   state = {
+//     image: '',
+//     data: [],
+//     status: 'idle',
+//     page: 1,
+//     error: null,
+//     showModal: false,
+//     currImg: {},
+//   };
+//   componentDidUpdate(prevProps, prevState) {
+//     const { page, image } = this.state;
+
+//     if (prevState.image !== image) {
+//       this.setState({ status: 'pending', page: 1 });
+//       api
+//         .fetchImage(image, page)
+//         .then(data => data.hits)
+//         .then(images => {
+//           this.setState({ data: images, status: 'resolved' });
+//         })
+//         .catch(error => this.setState({ error, status: 'rejected' }));
+//     }
+
+//     if (prevState.page !== page) {
+//       this.setState({ status: 'pending' });
+
+//       api
+//         .fetchImage(image, page)
+//         .then(data => data.hits)
+//         .then(images =>
+//           this.setState(prevState => ({
+//             data: [...prevState.data, ...images],
+//             status: 'resolved',
+//           })),
+//         )
+//         .catch(error => this.setState({ error, status: 'rejected' }));
+//     }
+//   }
+//   handleFormSubmit = image => {
+//     this.setState({ image });
+//   };
+
+//   onLoadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   toggleModal = image => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//       currImg: image,
+//     }));
+//   };
+
+//   render() {
+//     const { status, data, currImg } = this.state;
+//     return (
+//       <div>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         {status === 'idle' && <div>Waiting for your request</div>}
+
+//         {status === 'pending' && (
+//           <div>
+//             <Loader />
+//           </div>
+//         )}
+
+//         {status === 'resolved' && (
+//           <div>
+//             <ImageGallery data={data} onOpenModal={this.toggleModal} />
+//             {data.length > 0 && <Button onLoadMore={this.onLoadMore} />}
+//           </div>
+//         )}
+
+//         {status === 'rejected' && <div> Something went wrong</div>}
+//         {this.state.showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <img src={currImg.largeImageURL} alt={currImg.tags} />
+//           </Modal>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+// App.propTypes = {
+//   image: PropTypes.string,
+//   data: PropTypes.array,
+//   status: PropTypes.string,
+//   page: PropTypes.number,
+//   error: PropTypes.bool,
+//   showModal: PropTypes.bool,
+//   currImg: PropTypes.object,
+//   handleFormSubmit: PropTypes.func,
+//   onLoadMore: PropTypes.func,
+//   toggleModal: PropTypes.func,
+// };
+
+// useEffect(() => {
+
+//   if (!image) {
+//     return
+//   }
+
+//   async function getFetchImages() {
+//     setStatus('pending');
+//     try {
+//       const hits = await fetchImage(image, page);
+//       if (!hits.length) {
+//         throw new Error();
+//       }
+//       setData(prev => [...prev, ...data]);
+//       setStatus('resolved');
+
+//       page > 1 &&
+//         window.scrollTo({
+//           top: document.documentElement.scrollHeight,
+//           behavior: 'smooth',
+//         });
+//     } catch (error) {
+//       setError(error);
+//       setStatus('rejected');
+//     }
+//   }
+//   getFetchImages();
+// }, [image, page]);
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,36 +1,68 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, Modalstyle } from './Modal.styled';
+import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export default function Modal({ onClose, children }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = event => {
-    if (event.currentTarget === event.target) {
-      this.props.onClose();
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <Modalstyle>{this.props.children}</Modalstyle>
-      </Overlay>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <Modalstyle>{children}</Modalstyle>
+    </Overlay>,
+    modalRoot,
+  );
 }
+Modal.propTypes = {
+  onClose: PropTypes.func,
+};
+
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.handleBackdropClick}>
+//         <Modalstyle>{this.props.children}</Modalstyle>
+//       </Overlay>,
+//       modalRoot,
+//     );
+//   }
+// }
